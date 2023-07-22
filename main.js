6aaee9b0ecb0ddeada4bfdca44668bd5d705e779
imkuljeet <iamkkashyap@gmail.com>
2023-07-22 20:24:47 +0530

    Added Delete Button on the list

--- main.js
+++ main.js
@@ -13,6 +13,14 @@ function addItem(e){
     var li = document.createElement('li');
     li.className = 'list-group-item';
     li.appendChild(document.createTextNode(newItem));
+    
+    
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('x'));
+    li.appendChild(deleteBtn);
+
+
     itemList.appendChild(li);
 }
 
